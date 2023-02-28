3d0e2109ab2ee46cef3160d5d345486381020d8a
glitch <glitch@users.noreply.github.com>
2023-02-28 15:25:37 -0500

    Pretty up the OpenApi doc strings

--- src/main/java/dev/glitch/exploratory/spring/SpringBootApp.java
+++ src/main/java/dev/glitch/exploratory/spring/SpringBootApp.java
@@ -3,7 +3,18 @@ package dev.glitch.exploratory.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+  info = @Info(
+    title = "Exploratory Lookup-o-matic!",
+    description = "Did you ever want to build a Lookup Service with FSTs (Lucene) or BloomFilters (Guava) backed by Sorted K,V pair compressed files? ME TOO!",
+    contact = @Contact(name = "Github", url = "https://github.com/glitch/glitch-exploratory-lookomatic")
+  )
+)
 public class SpringBootApp {
 
   public static void main(String[] args) {
